b27a2f04224e59d2021bedec610ca88d298a265e
PaulUpam <paulupam@gmail.com>
2023-12-27 00:08:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-center text-4xl bg-blue-600">React Tailwind DaisyUi install done</h1>
+
+   
+    </>
+  )
+}
+
+export default App
