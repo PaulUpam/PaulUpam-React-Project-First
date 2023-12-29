34812574fff6992d37b3788c46bf94f5b59b5c80
PaulUpam <paulupam@gmail.com>
2023-12-29 21:51:58 +0600

    c panel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <h1 className="text-center text-4xl bg-blue-600">React Tailwind DaisyUi install done</h1>
-
+<h2 className="text-center text3xl bg-red-500">Cpanel deploy done</h2>
    
     </>
   )
